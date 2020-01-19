bb70a39237bf126332e1efd49d4b206841b04f6e
Rafael Macalaba <rafael.macalaba@yahoo.com>
2020-01-19 13:13:25 +0800

    pred to title
    prediction to title

--- app/server.py
+++ app/server.py
@@ -62,8 +62,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    prediction = prediction.replace("_"," ").title()
-    return JSONResponse({'result': str(prediction)})
+    return JSONResponse({'result': str(prediction).replace("_"," ").title()})
 
 
 if __name__ == '__main__':
